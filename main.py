f866572a0393e202da908833be4f9f739ff71cda
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-09-30 20:09:56 -0700

    get_file_lines is working

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 line_list  = []
-file = open("SILOV.txt", "r")
+infile = open('SILOV.txt', 'r')
+print("Name of file: ", infile.name)
+Lines = infile.readlines()
+#print(line)
 
-get_file_lines(filename):
-    return line_list
 
-lines_printed_backwards(line_list):
+def get_file_lines(filename):
+    for line in Lines:
+        line_list.append(line)
+    print(line_list)
+        
+    #for line in :
+     #   return line_list
+     #   print(line_list)
+     #   print("success")
+get_file_lines(infile)
+
+
+
+#def lines_printed_backwards(line_list):
+   # print
+
+#def lines_printed_random()
+
+#def lines_printed_custom()
+
+
+#if __name__ == "__main__":
+   # lines_printed_backwards(line_list)
+    #lines_printed_random(line_list)
+    #lines_printed_custom(line_list)
