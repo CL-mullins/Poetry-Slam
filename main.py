390406b6998c3e4d0936049ab99f25e7494975e7
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-10-01 12:27:25 -0700

    lines printed random & reverse working
    implemented two new functions.

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-
-line_list  = []
-infile = open('SILOV.txt', 'r') 
-Lines = infile.read().splitlines()
-print(Lines)
-
+import random
 
 def get_file_lines(filename):
-    for line in Lines:
-        line_list.append(line)
-    return line_list
+    infile = open(filename, 'r') 
+    lines = infile.read().splitlines()
+    #for line in lines:
+     #   line_list.append(line)
+    return lines
 
         
       
@@ -17,20 +14,44 @@ def get_file_lines(filename):
      #   print(line_list)
      #   print("success")
 
-def lines_printed_backwards(list):
-    for line, a in enumerate(list):
-        print("{0}. {1}".format(line,a))
+#num_list = list(enumerate(line_list,1))
+#TODO: Give each line a line number
+def lines_printed_backwards(lines_list):
+    line_length = len(lines_list)
+    lines_list.reverse()
+    for i in range(line_length):
+        print(f"{line_length - i} {lines_list[i]}")
+
+def lines_printed_random(lines_list):
+    line_length = len(lines_list)
+    for i in range(line_length):
+        print(lines_list[random.randint(0,line_length)])
+        
+
+        
+
+
+        
+
     #    line_list.insert(1 * line)
     #print(list.reverse)
     
-    
+#num_list.reverse()   
 
 #def lines_printed_random()
 
 #def lines_printed_custom()
-get_file_lines(infile)
-for line in line_list:
-    print(line)
+
+#for line in line_list:
+ #   print(line)
+lines_list = get_file_lines('SILOV.txt')
+#print(lines_list)
+lines_printed_backwards(lines_list)
+lines_printed_random(lines_list)
+
+#lines_printed_backwards(line_list)
+#lines_printed_backwards(line_list)
+#lines_printed_backwards(line_list)
 
 #lines_printed_backwards(line_list)
 
