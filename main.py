16fcac6ea5abb1bca8c2c2fc0b7247918515c1f6
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-10-01 10:53:21 -0700

    fixed encoding issue
    replaced apostrophe to avoid annoying issue

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 
 line_list  = []
-infile = open('SILOV.txt', encoding='utf-8') 
-Lines = infile.readlines()
+infile = open('SILOV.txt', 'r') 
+Lines = infile.read().splitlines()
+print(Lines)
 
 
 def get_file_lines(filename):
     for line in Lines:
         line_list.append(line)
     return line_list
+
         
-        
+      
     #for line in :
      #   return line_list
      #   print(line_list)
      #   print("success")
 
-#def lines_printed_backwards(list):
- #   reverse_list = line_list.reverse()
-  #  print(reverse_list)
+def lines_printed_backwards(list):
+    for line, a in enumerate(list):
+        print("{0}. {1}".format(line,a))
+    #    line_list.insert(1 * line)
+    #print(list.reverse)
+    
     
 
 #def lines_printed_random()
 
 #def lines_printed_custom()
 get_file_lines(infile)
-print(line_list)
+for line in line_list:
+    print(line)
+
+#lines_printed_backwards(line_list)
 
 #if __name__ == "__main__":
    # lines_printed_backwards(line_list)
