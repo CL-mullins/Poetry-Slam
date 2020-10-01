2e7f1820930f459fff0737cf9c5414c29092b8a0
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-10-01 12:46:20 -0700

    finished all 4 functions

--- main.py
+++ main.py
@@ -3,19 +3,10 @@ import random
 def get_file_lines(filename):
     infile = open(filename, 'r') 
     lines = infile.read().splitlines()
-    #for line in lines:
-     #   line_list.append(line)
     return lines
 
         
-      
-    #for line in :
-     #   return line_list
-     #   print(line_list)
-     #   print("success")
 
-#num_list = list(enumerate(line_list,1))
-#TODO: Give each line a line number
 def lines_printed_backwards(lines_list):
     line_length = len(lines_list)
     lines_list.reverse()
@@ -26,6 +17,13 @@ def lines_printed_random(lines_list):
     line_length = len(lines_list)
     for i in range(line_length):
         print(lines_list[random.randint(0,line_length)])
+
+
+def lines_printed_custom(lines_list):
+    #Prints out each line that has the term 'mother, 'ocean, or 'father'
+    for line in lines_list:
+        if "Ocean" or "mother" or "father" in line:
+            print(line)
         
 
         
@@ -33,29 +31,12 @@ def lines_printed_random(lines_list):
 
         
 
-    #    line_list.insert(1 * line)
-    #print(list.reverse)
-    
-#num_list.reverse()   
 
-#def lines_printed_random()
-
-#def lines_printed_custom()
-
-#for line in line_list:
- #   print(line)
 lines_list = get_file_lines('SILOV.txt')
-#print(lines_list)
+print("      ")
 lines_printed_backwards(lines_list)
+print("      ")
 lines_printed_random(lines_list)
-
-#lines_printed_backwards(line_list)
-#lines_printed_backwards(line_list)
-#lines_printed_backwards(line_list)
-
-#lines_printed_backwards(line_list)
-
-#if __name__ == "__main__":
-   # lines_printed_backwards(line_list)
-    #lines_printed_random(line_list)
-    #lines_printed_custom(line_list)
+print("      ")
+lines_printed_custom(lines_list)
+print("      ")
